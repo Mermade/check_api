2dd6806a8581409b51fac3d1b1dca7b55b1a972e
Mike Ralphson <mike.ralphson@gmail.com>
2017-08-10 07:57:23 +0100

    Allow fetching main url from self-signed https cert

--- check_api.js
+++ check_api.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var https = require('https');
 var url = require('url');
 var util = require('util');
 
@@ -20,7 +21,11 @@ function result(err,converted) {
 process.exitCode = 1;
 var u = url.parse(process.argv[2]);
 if (u.protocol) {
-	fetch(process.argv[2])
+	var options = {};
+	if (u.protocol.startsWith('https')) {
+		options.agent = new https.Agent({rejectUnauthorized: false});
+	}
+	fetch(process.argv[2],options)
 	.then(res => {
 		return res.text();
 	})
